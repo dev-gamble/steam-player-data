bd6529d47a122f3cd1e967a60aad662de85eeda8
Cory Gamble <fiendfriendly@proton.me>
2025-04-04 19:45:42 -0400

    fixed axios url source

--- src/client/services/steam.api.ts
+++ src/client/services/steam.api.ts
@@ -1,52 +1,55 @@
 import axios from 'axios';
 import { Achievement, Friend, PlayerSummary } from '../models';
 
-const api = "http://localhost:3001/api/steam";
+const api = axios.create({
+    baseURL: 'http://localhost:3001/api/steam',
+});
+  
 
 // Friends
 export const getFriends = async (steamid: string): Promise<Friend[]> => {
-    const res = await axios.get(`${api}/friends`, { params: steamid });
+    const res = await api.get('/friends', { params: steamid });
     return res.data;
 }
 
 // Game Achievements
 export const getGameAchievements = async (appid: number, steamid: string): Promise<Achievement[]> => {
-    const res = await axios.get(`${api}/game-achievements`, { params: { appid, steamid } });
+    const res = await api.get('/game-achievements', { params: { appid, steamid } });
     return res.data;
 }
 
 // Game Details
 export const getGameDetails = async (appid: number) => {
-    const res = await axios.get(`${api}/game-details`, { params: appid });
+    const res = await api.get('/game-details', { params: appid });
     return res.data;
 }
 
 // Game Stats
 export const getGameStats = async (appid: number, steamid: string) => {
-    const res = await axios.get(`${api}/game-stats`, { params: { appid, steamid } });
+    const res = await api.get('/game-stats', { params: { appid, steamid } });
     return res.data;
 }
 
 // Owned Games
 export const getOwnedGames = async (steamid: string) => {
-    const res = await axios.get(`${api}/owned-games`, { params: { steamid } });
+    const res = await api.get('/owned-games', { params: { steamid } });
     return res.data;
 }
 
 // Player Summary
 export const getPlayerSummary = async (steamid: string): Promise<PlayerSummary> => {
-    const res = await axios.get(`${api}/player-summary`, { params: steamid });
+    const res = await api.get('/player-summary', { params: steamid });
     return res.data.players[0];
 }
 
 // Recently Played Games
 export const getRecentlyPlayedGames = async (steamid: string) => {
-    const res = await axios.get(`${api}/recently-played-games`, { params: steamid });
+    const res = await api.get('/recently-played-games', { params: steamid });
     return res.data;
 }
 
 // Steam ID
 export const getSteamId = async (vanityurl: string) => {
-    const res = await axios.get(`${api}/steamid`, { params: { vanityurl } });
+    const res = await api.get('/steamid', { params: { vanityurl } });
     return res.data;
 }
